242e104e0d14df1366a542a908ba955f1d365799
timnoot <82733075+timnoot@users.noreply.github.com>
2024-06-28 18:33:39 +0200

    improved markdown

--- antenna-website/components/MarkdownRenderer.js
+++ antenna-website/components/MarkdownRenderer.js
@@ -1,22 +1,38 @@
-// components/MarkdownRenderer.js
-import { useEffect, useState } from 'react';
-import { remark } from 'remark';
-import html from 'remark-html';
+// // components/MarkdownRenderer.js
+// import { useEffect, useState } from 'react';
+// import { remark } from 'remark';
+// import html from 'remark-html';
+
+// const MarkdownRenderer = ({ markdown }) => {
+//     const [content, setContent] = useState('');
+
+//     useEffect(() => {
+//         const renderMarkdown = async () => {
+//             const processedContent = await remark().use(html).process(markdown);
+//             setContent(processedContent.toString());
+//         };
+
+//         renderMarkdown();
+//     }, [markdown]);
+
+//     return <div className="markdown text-text" dangerouslySetInnerHTML={{ __html: content }} />;
+// };
+
+
+// export default MarkdownRenderer;
+
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import rehypeRaw from 'rehype-raw';
 
 const MarkdownRenderer = ({ markdown }) => {
-    const [content, setContent] = useState('');
-
-    useEffect(() => {
-        const renderMarkdown = async () => {
-            const processedContent = await remark().use(html).process(markdown);
-            setContent(processedContent.toString());
-        };
-
-        renderMarkdown();
-    }, [markdown]);
-
-    return <div className="markdown text-text" dangerouslySetInnerHTML={{ __html: content }} />;
+    return (
+        <div className="markdown text-text">
+            <ReactMarkdown rehypePlugins={[rehypeRaw]}>
+                {markdown}
+            </ReactMarkdown>
+        </div>
+    );
 };
-
 
 export default MarkdownRenderer;
--- antenna-website/components/NavigationBar.js
+++ antenna-website/components/NavigationBar.js
@@ -3,13 +3,13 @@ import Link from "next/link";
 
 const NavigationBar = () => {
 	return (
-		<div className="bg-primary border-border border-2 p-4 w-full flex justify-center items-center h-[82px]">
+		<div className="bg-primary border-border border-2 p-4 w-full flex justify-center items-center h-[82px] relative">
 			<Link href="/" className="relative flex items-center justify-center">
 				<h1 className="text-2xl lg:text-4xl font-bold text-center">
 					Welcome to the NOA Satellite Tracker Website
 				</h1>
 			</Link>
-			<Link href="/" className="fixed left-2 top-0">
+			<Link href="/" className="absolute left-2 top-0">
 				<Image
 					src="/satellite.svg"
 					alt="Project Logo"
@@ -19,7 +19,7 @@ const NavigationBar = () => {
 					className="m-4"
 				/>
 			</Link>
-			<Link href="https://www.lis.nl/" className="fixed right-2 top-0">
+			<Link href="https://www.lis.nl/" className="absolute right-2 top-0">
 				<Image
 					src="/lis.png"
 					alt="LiS Logo"
@@ -29,7 +29,6 @@ const NavigationBar = () => {
 					className="m-4"
 				/>
 			</Link>
-
 		</div>
 	);
 };
