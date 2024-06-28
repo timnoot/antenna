74842321dd4f73b36fd9fa91032884a0c4ec45f1
timnoot <82733075+timnoot@users.noreply.github.com>
2024-06-28 17:57:13 +0200

    Update control.js

--- antenna-website/pages/control.js
+++ antenna-website/pages/control.js
@@ -3,18 +3,22 @@ import { useEffect, useRef, useState } from 'react';
 import ArduinoControl from "../components/ArduinoControl";
 
 const SATELLITES = [
-    { "name": "NOAA 19", "norad_id": 33591 },
-    { "name": "NOAA 18", "norad_id": 28654 },
-    { "name": "NOAA 17", "norad_id": 27453 },
-    { "name": "NOAA 15", "norad_id": 25338 },
-    { "name": "ISS", "norad_id": 25544 },
-    { "name": "NOAA 20", "norad_id": 43013 },
+    { "name": "NOAA 19", "norad_id": 33591, "freq": '137.100 MHz' },
+    { "name": "NOAA 18", "norad_id": 28654, "freq": '137.9125 MHz' },
+    { "name": "Meteor M2-4", "norad_id": 59051, "freq": '137.100-137.900 MHz' },
+    { "name": "Meteor M2-3", "norad_id": 57166, "freq": '137.100-137.900 MHz' },
+    { "name": "Meteor M2-2", "norad_id": 44387, "freq": '137.100-137.900 MHz' },
+    { "name": "Meteor M2", "norad_id": 40069, "freq": '137.100-137.900 MHz' },
+
+    { "name": "ISS", "norad_id": 25544, "freq": '145.825 MHz' },
 ]
 
 
 export default function Control() {
     const [norad_id, setNorad_id] = useState(33591);
     const [satname, setSatname] = useState('NOAA 19');
+    const [freq, setFreq] = useState('137.100 MHz');
+
     const [azimuth, setAzimuth] = useState(0);
     const [elevation, setElevation] = useState(0);
 
@@ -23,12 +27,18 @@ export default function Control() {
             <h1 className='text-4xl mt-8'>Control Page</h1>
             <div className='flex w-full justify-between'>
                 <div className='flex flex-col items-center w-[600px] min-w-[300px] ml-2 xl:ml-16'>
-                    <select className=' bg-secondary border-2 border-border hover:brightness-125 text-3xl rounded-xl px-2 transition duration-300 ease-in-out' onChange={(e) => {
-                        setNorad_id(e.target.value);
-                        setSatname(SATELLITES.find(s => s.norad_id === parseInt(e.target.value)).name);
-                    }}>
-                        {SATELLITES.map(s => <option key={s.norad_id} value={s.norad_id}>{s.name}</option>)}
-                    </select>
+                    <div className="flex justify-between items-center w-full">
+                        <div className="flex-grow">
+                            <select className="bg-secondary border-2 border-border hover:brightness-125 text-3xl rounded-xl px-2 transition duration-300 ease-in-out" onChange={(e) => {
+                                setNorad_id(e.target.value);
+                                setSatname(SATELLITES.find(s => s.norad_id === parseInt(e.target.value)).name);
+                                setFreq(SATELLITES.find(s => s.norad_id === parseInt(e.target.value)).freq);
+                            }}>
+                                {SATELLITES.map(s => <option key={s.norad_id} value={s.norad_id}>{s.name}</option>)}
+                            </select>
+                        </div>
+                        <p className="text-xl">Frequency: {freq}</p>
+                    </div>
 
                     <div className='w-full m-4'>
                         <SatMap norad_id={norad_id} satname={satname} setAzimuth={setAzimuth} setElevation={setElevation} />
